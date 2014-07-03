4c405a878d6a49284e8b315c7f6cfede0b5322d1
unknown <ch1pm3up@gmail.com>
2014-07-04 00:24:11 +0300

    Initial project commit(everything is very messy and units can't move)

---
+++ app.js
@@ -0,0 +1,187 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+var routes = require('./routes');
+var user = require('./routes/user');
+var ss = require('./routes/server-socket');
+var http = require('http');
+var path = require('path');
+var WebSocketServer = require('ws').Server;
+
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+app.get('/', routes.index);
+app.get('/server-socket', ss.index);
+app.get('/users', user.list);
+
+
+
+var server = http.createServer(app);
+
+server.listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
+
+
+
+var players = Array();
+var playersIndex = -1;
+var clients = Array();
+
+
+var wss = new WebSocketServer({server: server});
+wss.on('connection', function(ws) {
+
+	ws.id = playersIndex++
+	clients.push(ws);
+
+  console.log('started client interval');
+
+	console.log("length: ", clients.length);
+
+  ws.on('message', function(e){
+  	console.log(e);
+
+  	var parsedData = JSON.parse(e);
+
+  	console.log(parsedData.action);
+
+  	switch(parsedData.action){
+  		case "ping":
+  			console.log('Pong')
+			sendExistingPlayers(ws);
+  			break;
+  		case "createPlayer":
+  			addPlayer(ws, parsedData.sprite);
+  			break;
+		case "movePlayer":
+  			updatePlayerPosition(ws, parsedData.currPlayerId);
+  			break;
+		case "purgePlayers":
+			purgePlayers(ws);
+  			break;
+  		default:
+  			console.log('Unknown action\n');
+
+  	}
+  	console.log(players);
+
+
+  });
+  ws.on('close', function() {
+  	deleteId = ws.id;
+	removePlayer(deleteId);
+  });
+
+});
+
+
+function Player(sprite){
+	tmp = sprite;
+	this.x = tmp.x;
+	this.y = tmp.y;
+	this.z = tmp.z;
+}
+
+function addPlayer(ws, sprite){
+	console.log('newPlayerConnected');
+	obj = new Player(sprite);
+	obj.id = playersIndex;
+	players.push(obj);
+	data = {
+		action: 'newPlayerConnected',
+		playerData: obj
+	}
+
+	for (var i = 0; i < wss.clients.length; i++) {
+		// console.log("Some ids: ", i, obj.id, playersIndex);
+		if(i!=playersIndex)
+			wss.clients[i].send(JSON.stringify(data));
+	}
+
+	data.action = 'newPlayerCreated';
+	console.log(JSON.stringify(data));
+	ws.send(JSON.stringify(data));
+}
+
+function sendExistingPlayers(ws){
+
+	data = {
+		action: 'addExistingPlayers',
+		players: players
+	}
+	ws.send(JSON.stringify(data));
+	// for (var i = players.length - 1; i >= 0; i--) {
+	// 	data = {
+	// 		action: 'newPlayerCreated',
+	// 		playerData: players[i]
+	// 	}
+	// 	ws.send(JSON.stringify(data));
+	// };
+}
+
+
+function updatePlayerPosition(ws, playerId) {
+	player = players[playerId];
+	console.log(player);
+	player.x+=1;
+	data = {
+		action: 'updatePlayerPositionViewers',
+		player: player
+	}
+
+
+	for (var i = 0; i < wss.clients.length; i++) {
+
+		if(i!=playerId)
+			wss.clients[i].send(JSON.stringify(data));
+	};
+
+	data.action = 'updatePlayerPosition';
+	ws.send(JSON.stringify(data));
+
+}
+
+function purgePlayers(ws) {
+	players = Array();
+	ws.send(JSON.stringify({
+		action:'purgePlayers'
+	}));
+}
+
+
+function removePlayer(id) {
+
+	players.splice(id,1);
+  	clients.splice(id,1);
+  	playersIndex--;
+
+  	console.log("some len: ", clients.length);
+	for(var i = 0; i<wss.clients.length; i++){
+
+		wss.clients[i].send(JSON.stringify({
+			action: "removePlayer",
+			playerId: id
+		}));
+	}
+}
---
+++ public/javascripts/socket.js
@@ -0,0 +1,286 @@
+
+var connection;
+
+var ship;
+var ship2;
+var players = Array();
+var currPlayer;
+var map;
+var layer;
+var cursors;
+var game;
+function closeConn () {
+	connection.close();
+}
+
+function openConn () {
+ 	connection = new WebSocket('ws://localhost:3000/server-socket');
+
+	 // When the connection is open, send some data to the server
+	connection.onopen = function () {
+		game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser-example', { preload: preload, create: create, update: update, render: render });
+	};
+
+	// Log errors
+	connection.onerror = function (error) {
+	  console.log('WebSocket Error ' + error);
+	};
+
+	connection.onclose = function (){
+		currPlayer.destroy();
+	}
+
+	// Log messages from the server
+	connection.onmessage = function (e) {
+	  	parsedData = JSON.parse(e.data);
+	  
+		switch(parsedData.action){
+			case "newPlayerCreated":
+				console.log('newPlayerCreated\n');
+				addPlayerToGame(parsedData.playerData);
+				break;
+			case "newPlayerConnected":
+				console.log('newPlayerConnected\n');
+				addNewlyConnectedPlayer(parsedData.playerData);
+				break;
+			case "addExistingPlayers":
+				console.log('addExistingPlayers\n');
+				addExistingPlayers(parsedData.players);
+				break;
+			case "updatePlayerPosition":
+				console.log('updatePlayerPosition\n');
+				updatePlayerPosition(parsedData.player);
+				break;
+			case "updatePlayerPositionViewers":
+				console.log('updatePlayerPosition\n');
+				updatePlayerPositionViewers(parsedData.player);
+				break;
+			case "purgePlayers":
+				console.log('purgePlayers\n');
+				purgePlayersServerResponse();
+				break;
+			case "removePlayer":
+				console.log('removePlayer\n');
+				removePlayer(parsedData.playerId);
+				break;
+			default:
+				console.log('Unknown action\n');
+	  	}
+	  
+	  // if(e.data=='move_left'){
+	  // 	ship.body.rotateLeft(100);
+	  // 	ship2.body.rotateLeft(100);
+	  // }
+	  // else if(e.data=='move_right'){
+	  // 	ship.body.rotateRight(100);
+	  // 	ship2.body.rotateRight(100);
+	  // }
+	  // else if(e.data=='move_forward'){
+	  // 	ship.body.thrust(400);
+	  // 	ship2.body.thrust(400);
+	  // }
+	  // else if(e.data=='reverse'){
+	  // 	ship.body.reverse(400);
+	  // 	ship2.body.reverse(400);
+	  // }
+	
+	};
+
+}
+
+
+function preload() {
+
+    game.load.tilemap('map', 'assets/tilemaps/maps/collision_test.json', null, Phaser.Tilemap.TILED_JSON);
+    game.load.image('ground_1x1', 'assets/tilemaps/tiles/ground_1x1.png');
+    game.load.image('walls_1x2', 'assets/tilemaps/tiles/walls_1x2.png');
+    game.load.image('tiles2', 'assets/tilemaps/tiles/tiles2.png');
+    game.load.image('ship', 'assets/sprites/thrust_ship2.png');
+
+}
+
+
+function create() {
+
+    game.physics.startSystem(Phaser.Physics.P2JS);
+
+    game.stage.backgroundColor = '#2d2d2d';
+
+    map = game.add.tilemap('map');
+
+    map.addTilesetImage('ground_1x1');
+    map.addTilesetImage('walls_1x2');
+    map.addTilesetImage('tiles2');
+    
+    layer = map.createLayer('Tile Layer 1');
+
+    layer.resizeWorld();
+
+    //  Set the tiles for collision.
+    //  Do this BEFORE generating the p2 bodies below.
+    map.setCollisionBetween(1, 12);
+
+    //  Convert the tilemap layer into bodies. Only tiles that collide (see above) are created.
+    //  This call returns an array of body objects which you can perform addition actions on if
+    //  required. There is also a parameter to control optimising the map build.
+    game.physics.p2.convertTilemap(map, layer);
+
+
+    connection.send(JSON.stringify({action:'ping'})); // Send the message 'Ping' to the server
+    addPlayerServer(Math.floor((Math.random() * 200) + 100), Math.floor((Math.random() * 200) + 100)); 
+
+
+    //  By default the ship will collide with the World bounds,
+    //  however because you have changed the size of the world (via layer.resizeWorld) to match the tilemap
+    //  you need to rebuild the physics world boundary as well. The following
+    //  line does that. The first 4 parameters control if you need a boundary on the left, right, top and bottom of your world.
+    //  The final parameter (false) controls if the boundary should use its own collision group or not. In this case we don't require
+    //  that, so it's set to false. But if you had custom collision groups set-up then you would need this set to true.
+    game.physics.p2.setBoundsToWorld(true, true, true, true, false);
+
+    //  Even after the world boundary is set-up you can still toggle if the ship collides or not with this:
+    // ship.body.collideWorldBounds = false;
+
+    cursors = game.input.keyboard.createCursorKeys();
+
+
+}
+
+function addPlayerServer(x, y){
+    data = {
+    	action: 'createPlayer',
+    	sprite: new RequestNewPlayer(x,y,0,0)
+    }
+	connection.send(JSON.stringify(data));
+}
+
+function removePlayer(id){
+	console.log(id, "Player to remove: ", players[id]);
+	players[id].destroy();
+	players.splice(id,1);
+}
+
+function addPlayerToGame(data) {
+	obj = game.add.sprite(data.x, data.y, 'ship');
+	obj.id = data.id;
+	console.log(obj);
+    game.physics.p2.enable(obj);
+    game.camera.follow(obj);
+    currPlayer = obj;
+	return currPlayer;
+}
+
+function addNewlyConnectedPlayer(data){
+	obj = game.add.sprite(data.x, data.y, 'ship');
+	obj.id = data.id;
+	game.physics.p2.enable(obj);
+    game.camera.follow(obj);
+    console.log(obj.id);
+	players[obj.id-1] = obj;
+	return players[obj.id];
+}
+
+function addExistingPlayers (playersData){
+	this.players = Array();
+	console.log(playersData)
+	for (var i = 0; i <= playersData.length - 1; i++) {
+		// console.log(players[i]);
+		var data = playersData[i];
+		obj = game.add.sprite(data.x, data.y, 'ship');
+		obj.id = data.id;
+		 console.log("adding player...");
+		 console.log(obj);
+		this.players.push(obj)
+	    game.physics.p2.enable(obj);
+	    game.camera.follow(obj);
+	};
+}
+
+function PhaserSpriteDataTransferObject(sprite){
+	this.x = sprite.x;
+	this.y = sprite.y;
+	this.z = sprite.z;
+	this.angle = sprite.angle;
+}
+
+function RequestNewPlayer(x,y,z,angle){	
+	this.x = x;
+	this.y = y;
+	this.z = z;
+	this.angle = angle;
+}
+
+function updatePlayerPosition(data){
+	currPlayer.x = data.x;
+	currPlayer.y = data.y;
+	console.log("Curr player: ", currPlayer, data);
+	//player.body.thrust(400);
+}
+
+
+
+function updatePlayerPositionViewers (playerId) {
+	
+	player = players[playerId.id-1];
+	
+	player.x = playerId.x;
+	player.y = playerId.y;
+}
+
+function purgePlayers(){
+	connection.send(JSON.stringify({action:"purgePlayers"}))
+}
+
+
+function purgePlayersServerResponse() {
+	for (var i = 0; i < players.length; i++) {
+		players[i].destroy();
+	};
+	players = Array();
+}
+
+
+function update() {
+
+    if (cursors.left.isDown)
+    {
+    	// connection.send('left');
+       // ship.body.rotateLeft(100);
+    }
+    else if (cursors.right.isDown)
+    {
+    	// connection.send('right');
+        // ship.body.rotateRight(100);
+    }
+    else
+    {
+    	// connection.send('stop');
+        //ship.body.setZeroRotation();
+        //ship2.body.setZeroRotation();
+    }
+
+    if (cursors.up.isDown)
+    {
+    	connection.send(JSON.stringify({action:'movePlayer', currPlayerId: currPlayer.id}))
+    	// connection.send('forward');
+        // ship.body.thrust(400);
+    }
+    else if (cursors.down.isDown)
+    {
+    	// connection.send('reverse');
+        // ship.body.reverse(400);
+    }
+
+}
+
+function render() {
+
+}
+
+
+$(document).ready(function(){
+
+	openConn();
+
+
+});
